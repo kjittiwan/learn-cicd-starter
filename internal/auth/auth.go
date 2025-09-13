6ffff5b5c2f9f2ad04c13379e96ab6b193216e42
Kann Jittiwan <kann.jittiwan@eyt3.com>
2025-09-13 09:02:07 +0700

    revert test failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -19,5 +19,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1], errors.New("test workflow fail")
+	return splitAuth[1], nil
 }
